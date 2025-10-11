210c5d214d8e5d42b8506b8186e1c91d76588ca4
Neha <nehay3051@gmail.com>
2025-10-11 16:28:10 +0530

    Update Course.jsx

--- frontend/vite-project/src/components/Course.jsx
+++ frontend/vite-project/src/components/Course.jsx
@@ -20,7 +20,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/book`);
+        const res = await axios.get("https://bookstoreapp-b.onrender.com");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {

70f7d7d792670ba4f486371836832b0e8c09d1d4
Neha <nehay3051@gmail.com>
2025-10-11 16:33:01 +0530

    Update Signup.jsx

--- frontend/vite-project/src/components/Signup.jsx
+++ frontend/vite-project/src/components/Signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
     };
 
     try {
-      const res = await axios.post("http://localhost:4000/user/signup", userInfo);
+      const res = await axios.post("https://bookstoreapp-b.onrender.com/user/signup", userInfo);
       if (res.data) {
         toast.success("Signup Successful");
         localStorage.setItem("Users", JSON.stringify(res.data.user));
